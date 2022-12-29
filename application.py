74fbe502d1019c3cf75a7aeacede5d7aef2cf665
FollowCrom <followcrom@gmail.com>
2022-12-29 12:30:15 +0000

    remote commit

---
+++ application.py
@@ -0,0 +1,36 @@
+"""
+setx FLASK_APP ".\application.py"
+"""
+
+from flask import Flask, render_template, jsonify, request
+from sqlalchemy import text
+from database import engine, load_domdoms_from_db, load_domdom_from_db, add_to_db
+import random
+
+application = Flask(__name__)
+
+
+@application.route('/')
+def show_dom():
+    domdoms = load_domdoms_from_db()
+    numdoms = len(domdoms)
+    id_range = [d['id'] for d in domdoms]
+    dom_id = random.choice(id_range)
+    domdom = load_domdom_from_db(dom_id)
+    return render_template('index.html',
+                           domdom=domdom, dom_id=dom_id,
+                           numdoms=numdoms)
+
+
+@application.route('/api')
+def show_json():
+    domdoms = load_domdoms_from_db()
+    return jsonify(domdoms)
+
+
+@application.route('/sent', methods=['post'])
+def send_wisdom():
+    data = request.form
+    add_to_db(data)
+    return render_template('submitted.html',
+                           submission=data)
---
+++ database.py
@@ -0,0 +1,38 @@
+import sqlalchemy
+from sqlalchemy import create_engine, text
+import random
+import os
+
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+
+engine = create_engine(db_connection_string)
+
+def load_domdoms_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from domdoms3")) # any sql query
+        domdoms_lst = []
+        for row in result.all():
+            domdoms_lst.append(dict(row))
+        return domdoms_lst
+
+
+def load_domdom_from_db(dom_id):
+    numdoms = len(load_domdoms_from_db())
+    with engine.connect() as conn:
+        result = conn.execute(
+        text("select * from domdoms3 where id = :val"),
+        val=dom_id
+        )
+        rows = result.all()
+        return dict(rows[0])
+
+
+
+def add_to_db(data):
+    with engine.connect() as conn:
+        query = text("INSERT INTO domdoms3 (title, wisdom) VALUES (:title, :wisdom)")
+        conn.execute(query,
+        title=data['title'],
+        wisdom=data['wisdom']
+        )
