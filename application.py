62f36506065cad417de7f5fa924455bb0f3fe967
FollowCrom <followcrom@gmail.com>
2023-06-12 16:48:30 +0100

    application run

--- application.py
+++ application.py
@@ -1,7 +1,3 @@
-"""
-setx FLASK_APP ".\application.py"
-"""
-
 from flask import Flask, render_template, jsonify, request
 from sqlalchemy import text
 from database import engine, load_domdoms_from_db, load_domdom_from_db, add_to_db
@@ -10,27 +6,38 @@ import random
 application = Flask(__name__)
 
 
-@application.route('/')
+@application.route("/")
 def show_dom():
     domdoms = load_domdoms_from_db()
     numdoms = len(domdoms)
-    id_range = [d['id'] for d in domdoms]
+    id_range = [d["id"] for d in domdoms]
     dom_id = random.choice(id_range)
     domdom = load_domdom_from_db(dom_id)
-    return render_template('index.html',
-                           domdom=domdom, dom_id=dom_id,
-                           numdoms=numdoms)
+    return render_template("index.html", domdom=domdom, dom_id=dom_id, numdoms=numdoms)
 
 
-@application.route('/api')
+@application.route("/api")
 def show_json():
     domdoms = load_domdoms_from_db()
     return jsonify(domdoms)
 
 
-@application.route('/sent', methods=['post'])
+@application.route("/sent", methods=["post"])
 def send_wisdom():
     data = request.form
     add_to_db(data)
-    return render_template('submitted.html',
-                           submission=data)
+    return render_template("submitted.html", submission=data)
+
+
+if __name__ == "__main__":
+    application.run()
+
+
+# if __name__ == "__main__":
+#     from waitress import serve
+
+#     serve(application, host="http://217.160.156.18/", port=5000)
+
+
+# if __name__ == "__main__":
+#     application.run(host="127.0.0.1", port=80, debug=True)
