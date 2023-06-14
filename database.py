426cd0b3a256e4ea9800268c8d68f7b431ff95e6
FollowCrom <followcrom@gmail.com>
2023-06-14 18:14:42 +0100

    no honeypot

--- database.py
+++ database.py
@@ -43,12 +43,7 @@ def load_domdom_from_db(dom_id):
 
 
 def add_to_db(data):
-    if not data.get("honeypot"):
-        with engine.connect() as conn:
-            query = text(
-                "INSERT INTO domdoms3 (title, wisdom) VALUES (:title, :wisdom)"
-            )
-            conn.execute(query, {"title": data["title"], "wisdom": data["wisdom"]})
-        return True
-    else:
-        return False
+    with engine.connect() as conn:
+        query = text("INSERT INTO domdoms3 (title, wisdom) VALUES (:title, :wisdom)")
+        conn.execute(query, {"title": data["title"], "wisdom": data["wisdom"]})
+    return True
